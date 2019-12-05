7ad06e51bc8d9258b6f47919cca0258764c47a6b
Gordon <hellogordon.f@gmail.com>
2019-12-05 16:39:28 +0800

    Initial Commit

---
+++ FindGoodCafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindGoodCafe
+//
+//  Created by Kuan-Chieh Feng on 2019/12/5.
+//  Copyright © 2019 Kuan-Chieh Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
